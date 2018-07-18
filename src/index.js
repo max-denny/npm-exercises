7a1786a9981f0b976f514c76288fa938681f3ec6
Robin Maxwell <robinmaxwell@Robin-Maxwells-MacBook-Pro.local>
2018-07-18 12:09:18 -0500

    NPM/ Modules Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const $ = require('jquery');
+
+import lovelyMessage from '../say-hello.js';
+
+$('body').css('background-color', 'red');
+
+lovelyMessage('hello');
+
+//$
+
---
+++ src/say-hello.js
@@ -0,0 +1,5 @@
+"use strict";
+
+export const lovelyMessage = (message) => console.log(message);
+
+//lovelyMessage('Hey this is pretty cool!');
